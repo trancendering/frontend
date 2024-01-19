b8a3fc9040642bce0fd28309ecce656f7491014d
gerry-mandering <kimmen1119@gmail.com>
2024-01-19 18:57:56 +0900

    Feat: loginButton.js, login.js, languageSelector.js - add language change

--- src/views/components/login.js
+++ src/views/components/login.js
@@ -14,11 +14,26 @@ export default class Login extends Component {
 
 	async render() {
 		console.log("render login page");
+
+		const languageId = store.state.languageId;
+
+		const languageSet = {
+			en : {
+				title: "Login",
+			},
+			ko : {
+				title: "로그인",
+			},
+			ch : {
+				title: "登录",
+			},
+		}
+
 		const view = `
             <div class="d-flex align-items-center justify-content-center vh-100 bg-light">
                   <div class="card shadow p-3 mb-5 bg-white rounded" style="max-width: 18rem;">
                       <div class="card-body text-center">
-                          <h1 class="h3 mb-3 font-weight-bold">Login</h1>
+                          <h1 class="h3 mb-3 font-weight-bold">${languageSet[languageId].title}</h1>
                       </div>
                       <div id="login-btn"></div>
                   </div>
