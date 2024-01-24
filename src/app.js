8f5a66305b9da844fd1264e1dba94a974467d423
hey-uu <khy2714@gmail.com>
2024-01-25 04:23:13 +0900

    Feat: /game으로 직접 접근 시 제어

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-import router, {navigateTo} from "./views/utils/router.js";
+import router, { navigateTo } from "./views/utils/router.js";
+import store from "./store/index.js";
 
 window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", (e) => {
-        const targetElement = e.target.closest("[data-link]");
+	document.body.addEventListener("click", (event) => {
+		const targetElement = event.target.closest("[data-link]");
 
         if (targetElement) {
-            e.preventDefault();
+			event.preventDefault();
             navigateTo(targetElement.href);
         }
     });
 
+    if (store.state.gameStatus !== "playing"
+        && window.location.pathname === "/game") {
+		navigateTo("/");
+	} else {
     router();
+	}
 });
