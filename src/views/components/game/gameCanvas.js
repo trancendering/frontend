0fd6c0f33ebc496bad68577c0941c6c2d1f740db
gerry-mandering <kimmen1119@gmail.com>
2024-01-29 10:11:24 +0900

    Remove: gameCanvas.js - initStore() 메소드 삭제

--- src/views/components/game/gameCanvas.js
+++ src/views/components/game/gameCanvas.js
@@ -1,6 +1,6 @@
 import store from "../../../store/index.js";
 import Component from "../../../library/component.js";
-import {Side, Game} from "../../../enum/constant.js";
+import {Side} from "../../../enum/constant.js";
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import TWEEN from '@tweenjs/tween.js';
@@ -18,43 +18,16 @@ export default class gameCanvas extends Component {
             store,
             element: document.getElementById("gameCanvas"),
         });
-        this.initStore();
         this.render();
         this.handleEvent();
         store.events.subscribe("leftUserScoreChange", async () => this.updateLeftUserScore());
         store.events.subscribe("rightUserScoreChange", async () => this.updateRightUserScore());
-        // store.events.subscribe("ballPositionChange", async () => this.render());
-        // store.events.subscribe("scoreChange", async () => this.render());
-    }
-
-    // TODO ThreeJS 맞춰서 좌표계 수정하기 - 중심을 (0, 0)으로
-    /*
-     *	{
-     *		ballPosition: {
-     *	    	x: 0,
-     *	    	y: 0,
-     *	 	},
-     *	 	leftPaddlePosition: 0,
-     *	 	rightPaddlePosition: 0,
-     * 	}
-     */
-    initStore() {
-        store.dispatch("initPositions", {
-            ballPosition: {
-                x: Game.CANVAS_WIDTH / 2,
-                y: Game.CANVAS_HEIGHT / 2,
-            },
-            leftPaddlePosition: Game.CANVAS_HEIGHT / 2,
-            rightPaddlePosition: Game.CANVAS_HEIGHT / 2,
-        });
-        store.dispatch("initScores");
     }
 
     initRenderer() {
         this.renderer = new THREE.WebGLRenderer({canvas: this.element, antialias: true});
         this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
     }
 
     initCamera() {
@@ -112,7 +85,7 @@ export default class gameCanvas extends Component {
     }
 
     introAnimation() {
-        this.controls.enabled = false //disable orbit controls to animate the camera
+        this.controls.enabled = false
 
         const cameraTween = new TWEEN.Tween(this.camera.position.set(30, 30, -25))
             .to({x: 0, y: 0, z: 3.5}, 2500)
@@ -186,56 +159,4 @@ export default class gameCanvas extends Component {
         this.rightScoreObject = createScoreObject(store.state.rightUserScore, Side.RIGHT);
         this.scene.add(this.rightScoreObject);
     }
-
-    // async drawObjects() {
-    //     this.ctx.fillStyle = "#000";
-    //     this.ctx.fillRect(0, 0, Game.CANVAS_WIDTH, Game.CANVAS_HEIGHT);
-    //     this.drawPaddle("left");
-    //     this.drawPaddle("right");
-    //     this.drawBall();
-    // }
-    //
-    // async drawPaddle(side) {
-    //     const x = side === "left" ? 0 : Game.CANVAS_WIDTH - Game.PADDLE_WIDTH;
-    //     const y =
-    //         side === "left"
-    //             ? store.state.leftPaddlePosition
-    //             : store.state.rightPaddlePosition;
-    //
-    //     this.ctx.beginPath();
-    //     this.ctx.rect(
-    //         x,
-    //         y - Game.PADDLE_HEIGHT / 2,
-    //         Game.PADDLE_WIDTH,
-    //         Game.PADDLE_HEIGHT
-    //     );
-    //     this.ctx.fillStyle = "#FFF";
-    //     this.ctx.fill();
-    //     this.ctx.closePath();
-    // }
-    //
-    // async drawBall() {
-    //     const {x, y} = store.state.ballPosition;
-    //     this.ctx.beginPath();
-    //     this.ctx.arc(x, y, Game.BALL_RADIUS, 0, Math.PI * 2);
-    //     this.ctx.fillStyle = "#FFF";
-    //     this.ctx.fill();
-    //     this.ctx.closePath();
-    // }
-    //
-    // async drawScores() {
-    //     this.ctx.font = "20px Arial";
-    //
-    //     const gameInfo = store.state.gameInfo;
-    //     const leftDesignator = gameInfo.userSide === Side.LEFT ? "(Me)" : "";
-    //     const rightDesignator = gameInfo.userSide === Side.RIGHT ? "(Me)" : "";
-    //     const leftUserText = `${gameInfo.nickname[0]} ${leftDesignator}: ${store.state.leftUserScore}`;
-    //     const rightUserText = `${gameInfo.nickname[1]} ${rightDesignator}: ${store.state.rightUserScore}`;
-    //
-    //     const leftUserTextX = 30;
-    //     const rightUserTextX =
-    //         Game.CANVAS_WIDTH - 30 - this.ctx.measureText(rightUserText).width;
-    //     this.ctx.fillText(leftUserText, leftUserTextX, 30);
-    //     this.ctx.fillText(rightUserText, rightUserTextX, 30);
-    // }
 }
