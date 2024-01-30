91b946ec0b8a41f6dbe108e6a4ef4ea34584a56b
hey-uu <khy2714@gmail.com>
2024-01-30 12:03:17 +0900

    Feat: game, tournament, tournamentRecord 페이지 다중 언어 지원

--- src/views/components/tournamentRecord.js
+++ src/views/components/tournamentRecord.js
@@ -1,5 +1,6 @@
 import store from "../../store/index.js";
 import Component from "../../library/component.js";
+import { tournamentRecord } from "../utils/languagePack.js";
 
 export default class TournamentRecord extends Component {
 	constructor(params) {
@@ -44,6 +45,7 @@ export default class TournamentRecord extends Component {
 	}
 
 	async populateTournamentList() {
+		const languageId = store.state.languageId;
 		// Clear any existing content
 		// Parse the tournament log data and get the tournament list container
 		try {
@@ -73,7 +75,7 @@ export default class TournamentRecord extends Component {
 					tournamentItem.appendChild(gameList);
 				} else {
 					tournamentItem.innerHTML +=
-						"<p>No games in this tournament.</p>";
+						`<p>${tournamentRecord[languageId].noGame}.</p>`;
 				}
 				tournamentList.appendChild(tournamentItem);
 			});
@@ -83,17 +85,19 @@ export default class TournamentRecord extends Component {
 	}
 
 	async createGameItem(game, gameIndex) {
+		const languageId = store.state.languageId;
 		const gameItem = document.createElement("li");
+
 		gameItem.className = "list-group-item";
 		if (gameIndex === 2) {
 			// gameItem.classList.add('bg-secondary');
 			gameItem.classList.add("text-primary"); // Bootstrap 클래스를 사용하여 배경색 변경
 		}
-		const winnerTemplate = /*html*/ `<strong>Winner:</strong> <span class="name">${game.winner.name}</span>, <span class="score">Score: ${game.winner.score}</span>`;
-		const loserTemplate = /*html*/ `<strong>Loser:</strong> <span class="name">${game.loser.name}</span>, <span class="score">Score: ${game.loser.score}</span>`;
+		const winnerTemplate = /*html*/ `<strong>${tournamentRecord[languageId].winner}:</strong> <span class="name">${game.winner.name}</span>, <span class="score">${tournamentRecord[languageId].score}: ${game.winner.score}</span>`;
+		const loserTemplate = /*html*/ `<strong>${tournamentRecord[languageId].loser}:</strong> <span class="name">${game.loser.name}</span>, <span class="score">${tournamentRecord[languageId].score}: ${game.loser.score}</span>`;
 		gameItem.innerHTML = /*html*/ `
             <div class="game-item">
-                <div class="text-start game-id">Game ID: ${game.game_id}</div>
+                <div class="text-start game-id">${tournamentRecord[languageId].gameId} ${game.game_id}</div>
                 <div class="text-start winner">${winnerTemplate}</div>
                 <div class="text-start loser">${loserTemplate}</div>
             </div>
